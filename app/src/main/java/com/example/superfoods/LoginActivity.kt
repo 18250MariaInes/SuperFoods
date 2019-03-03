65654fe793c1596890758a13cccc30b10fe58613
18250MariaInes <35271285+18250MariaInes@users.noreply.github.com>
2019-03-03 13:20:07 -0600

    Login finalizado

---
+++ app/src/main/java/com/example/superfoods/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.superfoods
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var txtUser: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        txtUser=findViewById(R.id.txtUser)
+        txtPassword=findViewById(R.id.txtPassword)
+        progressBar= findViewById(R.id.progressBar2)
+        auth= FirebaseAuth.getInstance()
+    }
+    fun forgotPassword(view:View){
+        startActivity(Intent(this, ForgotPassActivity::class.java))
+    }
+    fun register(view:View){
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+    fun login(view:View){
+        loginUser()
+    }
+    private fun loginUser(){
+        val user:String=txtUser.text.toString()
+        val password:String=txtPassword.text.toString()
+
+        if (!TextUtils.isEmpty(user)&&!TextUtils.isEmpty(password)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user, password)
+                .addOnCompleteListener(this){
+                    task ->
+
+                    if (task.isSuccessful){
+                       action()
+                    }else{
+                        Toast.makeText(this, "Error al iniciar sesión (autenticación)", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
---
+++ app/src/main/java/com/example/superfoods/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.superfoods
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
